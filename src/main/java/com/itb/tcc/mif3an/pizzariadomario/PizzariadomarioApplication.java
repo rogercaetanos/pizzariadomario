131125c86ad729228d7e431163172e83bd0d8c34
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-02-13 20:14:11 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/tcc/mif3an/pizzariadomario/PizzariadomarioApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.mif3an.pizzariadomario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariadomarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariadomarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/tcc/mif3an/pizzariadomario/PizzariadomarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.mif3an.pizzariadomario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariadomarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

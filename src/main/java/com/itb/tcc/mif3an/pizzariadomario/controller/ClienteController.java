445b9401bdd6eb20aa5c7d0770e33d272deea391
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-05-05 20:12:47 -0300

    ajuste fino

--- src/main/java/com/itb/tcc/mif3an/pizzariadomario/controller/ClienteController.java
+++ src/main/java/com/itb/tcc/mif3an/pizzariadomario/controller/ClienteController.java
@@ -6,7 +6,9 @@ import com.itb.tcc.mif3an.pizzariadomario.auth.AuthenticationService;
 import com.itb.tcc.mif3an.pizzariadomario.auth.RegisterRequest;
 import com.itb.tcc.mif3an.pizzariadomario.model.entity.Cliente;
 import com.itb.tcc.mif3an.pizzariadomario.model.enums.TipoUsuario;
+import jakarta.annotation.security.PermitAll;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,8 @@ public class ClienteController {
         this.authenticationService = authenticationService;
     }
 
+    //@PermitAll
+   // @PreAuthorize("hasAuthority('USER_CREATE')")
     @PostMapping
     public ResponseEntity<AuthenticationResponse> registerCliente (@RequestBody RegisterRequest registerRequest) {
         registerRequest.setTipoUsuario(TipoUsuario.CLIENTE);
